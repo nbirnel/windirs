c4972c4e74b70ae90d7543f700f321bf3a9d3f9f
Noah Birnel <nbirnel@gmail.com>
2014-03-20 15:13:58 -0700

    pass win2cyg

--- lib/windirs.rb
+++ lib/windirs.rb
@@ -3,4 +3,30 @@
 
 module Windirs
 
+  class Path
+
+    def initialize path, cygdrive_prefix = '/cygdrive'
+      drive_re = Regexp.new '^((?<drive>[A-Z]):)?(?<dirs>[/\\\].*)$'
+
+      if drive_re =~ path
+        m = Regexp.last_match
+        @drive = m[:drive]
+        @dirs  = m[:dirs]
+      end
+    end
+
+    def cygwin cygdrive_prefix = '/cygdrive'
+      cygdrive = @drive ? "#{cygdrive_prefix}/#{@drive.downcase}" : nil
+      dirs = @dirs.gsub '\\', '/'
+      "#{cygdrive}#{dirs}"
+    end
+
+    def ruby_win
+    end
+
+    def windows
+    end
+
+  end
+
 end
--- spec/windirs_spec.rb
+++ spec/windirs_spec.rb
@@ -1,5 +1,29 @@
 require  "#{File.dirname(__FILE__)}/../lib/windirs"
 
 describe Windirs do
+  before do
+    dpath_strings = {
+      :cygpath  => '/cygdrive/c/Users/joe/Desktop',
+      :winpath  => 'C:\Users\joe\Desktop',
+      :rubypath => 'C:/Users/joe/Desktop',
+    }
+    @dpaths = Hash.new
+    dpath_strings.map{|k, v| @dpaths[k] = Windirs::Path.new(v)}
+
+    upath_strings = {
+      :unc      => '\\windomain\somedir\file',
+      :cygunc   => '//windomain/somedir/file',
+    }
+    @upaths = Hash.new
+    upath_strings.map{|k, v| @upaths[k] = Windirs::Path.new(v)}
+  end
+
+  describe Windirs::Path do
+
+    it 'converts from windows drive' do
+      @dpaths[:winpath].cygwin.should eq '/cygdrive/c/Users/joe/Desktop'
+    end
+
+  end
 
 end
